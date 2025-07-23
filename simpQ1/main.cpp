dee3ead0e4b6b16b95d952cc45f29600d6a6a1c5
munyajunior <ivojunior671@gmail.com>
2025-07-23 18:49:04 +0100

    ✨ feat(loop): print even numbers from 2 to 20
    - create a do-while loop to iterate from 1 to 10
    - multiply each number by 2 and print it to the console

---
+++ simpQ1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    number=1;
+    do{
+        cout<<2*number<<endl;
+        number++;
+    }while(number<=10);
+    return 0;
+}
